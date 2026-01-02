b9991d67e802351275c1c2d93aef61ccd7635e4f
Precipitating <92101891+Precipitating@users.noreply.github.com>
2026-01-02 22:54:14 +0000

    + Add basic PPM image gradient

---
+++ Raytracing/Source.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+{
+	// Image
+	int imageWidth = 256;
+	int imageHeight = 256;
+
+	// Render
+    std::cout << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+    for (int j = 0; j < imageHeight; j++)
+    {
+        for (int i = 0; i < imageWidth; i++)
+        {
+            auto r = double(i) / (imageWidth - 1);
+            auto g = double(j) / (imageHeight - 1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+
+
+	return 0;
+}
