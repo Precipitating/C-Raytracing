7d77e0a1c92de4e2350b730701d552df8c1313ce
Precipitating <92101891+Precipitating@users.noreply.github.com>
2026-01-03 20:06:24 +0000

    + Add surface normal + Render surface normals on a sphere

--- Raytracing/Source.cpp
+++ Raytracing/Source.cpp
@@ -3,20 +3,36 @@
 #include "vec3.h"
 #include "ray.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r)
+double hit_sphere(const point3& center, double radius, const ray& r)
 {
     vec3 oc = center - r.origin();
     auto a = dot(r.direction(), r.direction());
     auto b = -2.0 * dot(r.direction(), oc);
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant >= 0);
+    if (discriminant < 0)
+    {
+        return -1.0;
+    }
+    else 
+    {
+        return (-b - std::sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray& r)
 {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1, 0, 0);
+    // how far along the ray the hit occurs
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+
+    // sphere hit if t > 0
+    if (t > 0.0)
+    {
+        vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+
+        // sky gradient
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
 
     vec3 unitDirection = unit_vector(r.direction());
     auto a = 0.5 * (unitDirection.y() + 1.0);
