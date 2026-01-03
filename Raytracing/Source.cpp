c9958460d18697179e2a8fbc1932b6463d1cec99
Precipitating <92101891+Precipitating@users.noreply.github.com>
2026-01-03 03:35:12 +0000

    + Add sphere + Red ray color if hit sphere, resulting in a red spherical shape on image.ppm

--- Raytracing/Source.cpp
+++ Raytracing/Source.cpp
@@ -3,8 +3,21 @@
 #include "vec3.h"
 #include "ray.h"
 
+bool hit_sphere(const point3& center, double radius, const ray& r)
+{
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant >= 0);
+}
+
 color ray_color(const ray& r)
 {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unitDirection = unit_vector(r.direction());
     auto a = 0.5 * (unitDirection.y() + 1.0);
     // lerp formula
@@ -57,6 +70,7 @@ int main()
             ray r(cameraCenter, rayDirection);
 
             color pixelColor = ray_color(r);
+            write_color(std::cout, pixelColor);
         }
     }
 
